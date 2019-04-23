b69c95344a5ea74b743c17c47bd8baaa16394a8c
Isadora Cabral <44280710+isadoracabral@users.noreply.github.com>
2019-04-23 11:42:01 -0700

    Assignments 2/3
    views and templates.
    index and base files in templates folder
    migration, runserver
    http://127.0.0.1:8000/PythonApp/

--- PythonClub/PythonApp/views.py
+++ PythonClub/PythonApp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index (request):
-    return render(request, 'PythonClub/index.html')
+    return render(request, 'PythonApp/index.html')
