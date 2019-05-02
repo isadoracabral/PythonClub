d8c9134ed2dcfda5ce37612d0e736716febb1515
Isadora Cabral <44280710+isadoracabral@users.noreply.github.com>
2019-05-02 11:50:27 -0700

    Assignment 5
    Resource views. 
    changes on views, urls, base, resource

--- PythonClub/PythonApp/views.py
+++ PythonClub/PythonApp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Meeting, MeetingMinutes, Resource, Event
 
 # Create your views here.
 def index (request):
     return render(request, 'PythonApp/index.html')
+
+def getresource(request):
+    resource_list=Resource.objects.all()
+    return render(request, 'PythonApp/resource.html' ,{'resource_list' : resource_list})
