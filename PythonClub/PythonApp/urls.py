72cb425fdb360653b75e1cdcde7f878dc91e22ac
Isadora Cabral <44280710+isadoracabral@users.noreply.github.com>
2019-05-16 12:48:31 -0700

    6 - Details
    details added

--- PythonClub/PythonApp/urls.py
+++ PythonClub/PythonApp/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns=[
     path('', views.index, name='index'),
     path('resource/', views.getresource, name='resource'),
+    path('meeting/', views.getmeeting, name='meeting'),
+    path('meetingdetails/<int:id>', views.meetingdetails, name='meetingdetails'),
 ]
 
--- PythonClub/PythonApp/views.py
+++ PythonClub/PythonApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Meeting, MeetingMinutes, Resource, Event
 
 # Create your views here.
@@ -8,3 +8,11 @@ def index (request):
 def getresource(request):
     resource_list=Resource.objects.all()
     return render(request, 'PythonApp/resource.html' ,{'resource_list' : resource_list})
+
+def getmeeting(request):
+    meeting_list=Meeting.objects.all()
+    return render(request, 'PythonApp/meeting.html', {'meeting_list': meeting_list})
+
+def meetingdetails(request, id):
+    meet=get_object_or_404(Meeting, pk=id)
+    return render(request, 'PythonApp/meetingdetails.html') #, {'meet':meet} 
